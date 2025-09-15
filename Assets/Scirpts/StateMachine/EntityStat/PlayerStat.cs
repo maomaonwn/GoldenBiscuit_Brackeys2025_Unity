c008fe6cdc0520e3751241b3162c286a7271fc11
maomaoovo <maomaoovo@outlook.com>
2025-09-15 16:11:51 +0800

    feat: 增加动态更新的血量文本，更新HUD效果

--- Assets/Scirpts/StateMachine/EntityStat/PlayerStat.cs
+++ Assets/Scirpts/StateMachine/EntityStat/PlayerStat.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Scirpts.PlayerControl;
 using TMPro;
 using UnityEngine;
@@ -7,6 +8,8 @@ namespace Scirpts.EntityStat
     public class PlayerStat : EntityStat
     {
         Player player => GetComponent<Player>();
+
+        public TMP_Text healthText;
         
         public override void Die()
         {
@@ -15,13 +18,45 @@ namespace Scirpts.EntityStat
             //->Dead 切换到死亡状态 
             player.Die();
         }
-        
-        /// <summary>
-        /// 更新 UI 文本
-        /// </summary>
-        private void UpdateHealthUI()
-        {
 
+        public override void TakeDamage(int _damage)
+        {
+            base.TakeDamage(_damage);
+            
+            //更新血量UI
+            UpdateHealthUI(_damage);
+        }
+
+        /// <summary>
+        /// 更新血量 UI 
+        /// </summary>
+        private void UpdateHealthUI(int _damage)
+        {
+            StartCoroutine(HealthChangeAnim(CurrentHealth, CurrentHealth - _damage, .3f));
+        }
+
+        /// <summary>
+        /// 数字渐变地更新文本
+        /// </summary>
+        /// <param name="_oldValue"></param>
+        /// <param name="_newValue"></param>
+        /// <param name="_animDuration"></param>
+        /// <returns></returns>
+        IEnumerator HealthChangeAnim(int _oldValue, int _newValue,float _animDuration)
+        {
+            float timer = 0f;
+
+            while (timer < _animDuration)
+            {
+                timer += Time.deltaTime;
+                int displayValue = Mathf.RoundToInt(Mathf.Lerp(_oldValue, _newValue, timer / _animDuration));
+
+                //更新血量文本
+                healthText.text = $"Health: {displayValue.ToString()}";
+                yield return null;
+            }
+
+            healthText.text = $"Health: {_newValue.ToString()}";
         }
     }
 }
