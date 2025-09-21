4a4ea7f4e1f3cce53ca0fff3eeae56ecb038cfe6
maomaoovo <maomaoovo@outlook.com>
2025-09-21 23:26:31 +0800

    feat: 增加状态机禁用状态和重启的接口

--- Assets/Scirpts/StateMachine/StateMachine.cs
+++ Assets/Scirpts/StateMachine/StateMachine.cs
@@ -8,6 +8,8 @@ namespace Scirpts
     public class StateMachine
     {
         public EntityState currentState { get; private set; }
+        
+        public bool b_AllStatesDisabled { get; private set; }
 
         public void Initialize(EntityState _startState)
         {
@@ -20,6 +22,30 @@ namespace Scirpts
             currentState.OnExit();
             currentState = _newState;
             currentState.OnEnter();
+        }
+
+        /// <summary>
+        /// 禁用所有状态
+        /// </summary>
+        public void DisableAllStates()
+        {
+            //退出当前状态
+            currentState?.OnExit();
+            //清空状态
+            currentState = null;
+            
+            b_AllStatesDisabled = true;
+        }
+
+        /// <summary>
+        /// 重新启动状态机
+        /// </summary>
+        /// <param name="_startState"></param>
+        public void EnableStateMachine(EntityState _startState)
+        {
+            ChangeState(_startState);
+
+            b_AllStatesDisabled = false;
         }
     }
 }
